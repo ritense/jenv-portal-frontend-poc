ef8cd574494e1bcbf02c37a078d68eef18d76ab3
mi <maykel.ibrahim@ritense.com>
2024-10-17 11:58:49 +0200

    Added footer data

--- src/constants/footer-data.ts
+++ src/constants/footer-data.ts
@@ -2,37 +2,97 @@ import { PortalFooter } from "@nl-portal/nl-portal-user-interface";
 
 export const footerData: PortalFooter = [
   {
-    titleTranslationKey: "rotterdam",
+    titleTranslationKey: "over-deze-site",
     links: [
       {
-        linkTranslationKey: "goToRotterdam",
-        url: "https://www.rotterdam.nl/",
+        linkTranslationKey: "wat-is-mijnoverheid",
+        url: "https://mijn.overheid.nl/wat-is-mijnoverheid/",
         hrefLang: "nl",
       },
       {
-        linkTranslationKey: "goToRotterdam",
-        url: "https://www.rotterdam.nl/",
+        linkTranslationKey: "toegankelijkheid",
+        url: "https://mijn.overheid.nl/toegankelijkheid/",
+        hrefLang: "nl",
+      },
+      {
+        linkTranslationKey: "sitemap",
+        url: "https://mijn.overheid.nl/sitemap/",
+        hrefLang: "nl",
+      },
+    ],
+  },
+  {
+    titleTranslationKey: "gegevensverwerking",
+    links: [
+      {
+        linkTranslationKey: "veiligheid",
+        url: "https://mijn.overheid.nl/veiligheid/",
+        hrefLang: "nl",
+      },
+      {
+        linkTranslationKey: "privacy",
+        url: "https://mijn.overheid.nl/privacy/",
+        hrefLang: "nl",
+      },
+      {
+        linkTranslationKey: "wet-en-regelgeving",
+        url: "https://mijn.overheid.nl/wet-en-regelgeving/",
         hrefLang: "en",
       },
     ],
   },
   {
-    titleTranslationKey: "disclaimers",
+    titleTranslationKey: "service",
     links: [
       {
-        linkTranslationKey: "accessibility",
-        url: "https://www.rotterdam.nl/toegankelijkheid",
+        linkTranslationKey: "mededelingen",
+        url: "https://mijn.overheid.nl/mededelingen",
+        hrefLang: "nl",
+      },
+      {
+        linkTranslationKey: "herken-oplichting",
+        url: "https://mijn.overheid.nl/herken-oplichting/",
+        hrefLang: "nl",
+      },
+      {
+        linkTranslationKey: "vragen",
+        url: "https://mijn.overheid.nl/vragen/",
         hrefLang: "nl",
       },
       {
         linkTranslationKey: "contact",
-        url: "https://www.rotterdam.nl/nl/contact",
+        url: "https://mijn.overheid.nl/contact/",
         hrefLang: "nl",
       },
       {
-        linkTranslationKey: "contact",
-        url: "https://www.rotterdam.nl/en/contact",
+        linkTranslationKey: "klacht",
+        url: "https://mijn.overheid.nl/contact/klacht",
         hrefLang: "en",
+      },
+    ],
+  },
+  {
+    titleTranslationKey: "partners",
+    links: [
+      {
+        linkTranslationKey: "organisaties",
+        url: "https://mijn.overheid.nl/organisaties/",
+        hrefLang: "nl",
+      },
+      {
+        linkTranslationKey: "overheidNl",
+        url: "https://www.overheid.nl/",
+        hrefLang: "nl",
+      },
+      {
+        linkTranslationKey: "rijksoverheidNl",
+        url: "https://www.rijksoverheid.nl/",
+        hrefLang: "en",
+      },
+      {
+        linkTranslationKey: "mfo-burgers",
+        url: "https://www.rvig.nl/mfo-burgers",
+        hrefLang: "nl",
       },
     ],
   },
